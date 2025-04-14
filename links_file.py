9a1a5420dba60616ae3a481fdd9e656e26f953bf
Jothin Kumar <contact@jothin.tech>
2025-04-14 12:31:01 +0530

    prevent adding blank lines

--- links_file.py
+++ links_file.py
@@ -9,6 +9,7 @@ def filter_out_existing_links(links):
     with open("links.txt", "r") as file:
         existing_links = file.read().splitlines()
         new_links = [link for link in links if link not in existing_links]
-    with open("links.txt", "a") as file:
-        file.write("\n".join(new_links) + "\n")
+    if new_links:
+        with open("links.txt", "a") as file:
+            file.write("\n".join(new_links) + "\n")
     return new_links
