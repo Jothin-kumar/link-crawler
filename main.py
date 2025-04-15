94d42720adff9234abec6a44e41e13cbd732ecdf
Jothin Kumar <contact@jothin.tech>
2025-04-15 10:57:23 +0530

    run every 30 seconds and also logs

--- main.py
+++ main.py
@@ -2,19 +2,28 @@ from json import loads
 from crawl import crawl
 from links_file import filter_out_existing_links
 from notify import notify
+from time import sleep
+from datetime import datetime
 
 
 with open("config.json", "r") as file:
     data = loads(file.read())
 
-for page in data:
-    for match in page["matches"]:
-        new_links = filter_out_existing_links(
-            [*crawl(
-                page["url"],
-                startswith=match["startswith"],
-                endswith=match["endswith"]
-            )]
-        )
-        for link, detail in new_links:
-            notify(match.get("message") or page["message"], link, detail)
+def __main__():
+    for page in data:
+        for match in page["matches"]:
+            new_links = filter_out_existing_links(
+                [*crawl(
+                    page["url"],
+                    startswith=match["startswith"],
+                    endswith=match["endswith"]
+                )]
+            )
+            for link, detail in new_links:
+                notify(match.get("message") or page["message"], link, detail)
+
+if __name__ == "__main__":
+    while True:
+        __main__()
+        print("Last checked at:", datetime.now())
+        sleep(30)
--- notify.py
+++ notify.py
@@ -4,9 +4,10 @@ from os import getenv
 
 
 load_dotenv()
-def notify(message, url, detail):  # TODO
+def notify(message, url, detail):
     DiscordWebhook(
         url=getenv("DISCORD_WEBHOOK"),
         rate_limit_retry=True,
         content=f"{message}\n[{detail}]({url})",
     ).execute()
+    print(f"Notified: {message}\n{detail}\nLink: {url}")
